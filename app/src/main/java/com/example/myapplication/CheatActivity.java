d0e079b030076cbb55ebf269917b200cb2f650ba
Javernaut <javernaut@gmail.com>
2019-11-23 10:40:47 +0200

    Explicit action for displaying a correct answer

--- app/src/main/java/com/example/myapplication/CheatActivity.java
+++ app/src/main/java/com/example/myapplication/CheatActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class CheatActivity extends LoggingActivity {
@@ -14,12 +16,21 @@ public class CheatActivity extends LoggingActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
 
+        Button showCorrectAnswerButton = findViewById(R.id.show_answer_button);
+        showCorrectAnswerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showCorrectAnswer();
+            }
+        });
+    }
+
+    private void showCorrectAnswer() {
         Intent intent = getIntent();
         boolean correctAnswer = intent.getBooleanExtra(KEY_CORRECT_ANSWER, false);
 
         TextView correctAnswerView = findViewById(R.id.correct_answer);
         correctAnswerView.setText(String.valueOf(correctAnswer));
-
     }
 
     public static Intent makeIntent(Context context, boolean correctAnswer) {
