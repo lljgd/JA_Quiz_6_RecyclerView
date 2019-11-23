65820ca2dd23b554997fa1763040b1141962ef85
Javernaut <javernaut@gmail.com>
2019-11-23 10:11:37 +0200

    Adding new CheatActivity

---
+++ app/src/main/java/com/example/myapplication/CheatActivity.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+public class CheatActivity extends LoggingActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+    }
+}
