5d55141cc9874706e09ec3d395f858e70ee646f8
Javernaut <javernaut@gmail.com>
2019-11-30 20:08:47 +0200

    Making a dummy activity for a list of Questions

---
+++ app/src/main/java/com/example/myapplication/AllQuestionsActivity.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AllQuestionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO set layout with RecyclerView inside
+
+        // TODO set LayoutManager to the RecyclerView
+
+        // TODO pass an Adapter to the RecyclerView. Use MainActivity.QUESTION_BANK as an argument
+    }
+}
